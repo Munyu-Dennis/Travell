112317b07976f778d23f79fe9d5c54bbb2b2cd7c
Dennis Munyu <ituromunyu@gmail.com>
2020-02-05 11:57:17 +0300

    changed allowed_hosts

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://mighty-plains-73010.herokuapp.com']
+ALLOWED_HOSTS = ['mighty-plains-73010.herokuapp.com']
 
 
 # Application definition
